be284649f1b2081f32633ef2d6c53203431306d0
Aabhas-Pal <pal.aabhas.1@outlook.com>
2025-10-03 15:09:48 +0530

    Create task.py
    Day 001 - Band Name Generator 🎸
    ---------------------------------
    This script is part of the 100 Days of Code: Python Challenge.
    
    📌 Project Objective:
    - Generate a fun and unique band name for the user.
    - Combines the name of the city where the user grew up and their pet's name.
    
    🎯 Concepts Covered:
    - Printing output to the console
    - Taking user input with input()
    - String concatenation
    
    🚀 How to Run:
    $ python task.py

---
+++ Day-001-Band-Name/task.py
@@ -0,0 +1,6 @@
+#Hi Guys I'am Aabhas Pal this the the first project that I build in this journey of 100-days-challenge-of-pythone-code.
+#Stay connected with me!
+print("Welcome to the band name generator tool.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+print("Your band name could be:" + city_name + pet_name)
