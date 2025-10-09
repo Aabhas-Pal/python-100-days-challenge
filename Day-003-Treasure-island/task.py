b5d456ae0fd567e1d651b37ed7f8f67cfeef6493
AabhascodesT-T <pal.aabhas.1@outlook.com>
2025-10-09 15:37:55 +0530

    Create task.py

---
+++ Day-003-Treasure-island/task.py
@@ -0,0 +1,52 @@
+#Welcome to my 100 Days of python code challenge here is a project created by me Aabhas Pal.
+#Please go through the project and enjoy the journey with me.
+
+
+
+
+print(r'''
+ _                                     
+| |                                    
+| |_ _ __ ___  __ _ ___ _   _ _ __ ___ 
+| __| '__/ _ \/ _` / __| | | | '__/ _ \
+| |_| | |  __/ (_| \__ \ |_| | | |  __/
+ \__|_|  \___|\__,_|___/\__,_|_|  \___|
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice = input("You are at a crossroad choose your path \n type left or right: ")
+
+if choice == "left":
+    option = input("You are in front of a river choose one option\n wait for boat or swim.\n")
+    if option == "wait":
+        op = input("There are the doors in front of you choose one for treasure\n red, yellow and blue what's your choice. \n")
+        if op == "yellow":
+            print("Hurray! you got the treasure")
+        elif op == "red":
+            print("You burned crisp fighting with a flaming tiger.")
+        elif op == "blue":
+            print("You died while fighting with leviathan.")
+    elif option == "swim":
+        print("A bunch of hungry piranha's fed upon you.")
+elif choice == "right":
+    print("You died falling in a sink hole.")
